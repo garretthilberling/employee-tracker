c372ad6cc4e2e813c6fd51c7f14f08caeda44495
garretthilberling <ghilberling@knights.ucf.edu>
2022-03-14 18:58:08 -0400

    everything is done but the READMEgit add .

--- utils/programInit.js
+++ utils/programInit.js
@@ -87,7 +87,7 @@ function viewEmployees() {
   const sql = ` SELECT e.e_id as id, concat(e.first_name,' ', e.last_name) AS employee, e.role_title AS title, e.role_salary AS salary, e.dept_name AS department, 
                 CASE WHEN e.manager_id = e.e_id THEN concat('N/A') ELSE concat(m.first_name, ' ', m.last_name) END AS manager 
                 FROM (SELECT * FROM employees LEFT JOIN roles ON employees.role_id = roles.r_id LEFT JOIN departments ON roles.dept_id = departments.d_id) AS e, employees m 
-                WHERE m.e_id = e.manager_id; `;
+                WHERE m.e_id = e.manager_id `;
   db.query(sql, (err, rows) => {
     if (err) {
       console.log(err.message);
@@ -131,9 +131,11 @@ function viewEmployeesByDept() {
       ])
       .then(({ deptId }) => {
         const sql =
-        `SELECT e_id as id, first_name, last_name, role_title AS title, role_salary AS salary, manager_name FROM employees
-        LEFT JOIN roles ON employees.role_id = roles.r_id
-        WHERE dept_id = ?`;
+        ` SELECT e.e_id as id, concat(e.first_name,' ', e.last_name) AS employee, e.role_title AS title, e.role_salary AS salary, e.dept_name AS department, 
+          CASE WHEN e.manager_id = e.e_id THEN concat('N/A') ELSE concat(m.first_name, ' ', m.last_name) END AS manager 
+          FROM (SELECT * FROM employees LEFT JOIN roles ON employees.role_id = roles.r_id LEFT JOIN departments ON roles.dept_id = departments.d_id) AS e, employees m 
+          WHERE m.e_id = e.manager_id
+          AND dept_id = ? `;
         const query = [deptId+1]; 
         db.query(sql, query, (err, rows) => {
         if (err) {
@@ -300,26 +302,44 @@ function addEmployee() {
     });
   });
   // to get a list of managers for user to choose from
-  const getManagerList = new Promise((resolve, reject) => {
-    var managerArr = [];
-    const sql = `SELECT first_name, last_name FROM employees FULL OUTER JOIN ON employees.manager_id = employees.e_id`;
+  const getActiveManagerList = new Promise((resolve, reject) => {
+    var activeManagerArr = [];
+    const sql = ` SELECT DISTINCT concat(m.first_name, ' ', m.last_name) 
+                  AS manager FROM employees e, employees m 
+                  WHERE m.e_id = e.manager_id  `;
     db.query(sql, (err, rows) => {
       if (err) {
         console.log(err.message);
       }
       for (var i = 0; i < rows.length; i++) {
-        managerArr.push(
-          Object.values(rows[i])[0] + " " + Object.values(rows[i])[1]
-        ); // we need to get the first and last name
+        activeManagerArr.push(Object.values(rows[i])[0]);
       }
-      managerArr.push("Employee's manager not listed");
+      activeManagerArr.push("Show more");
+      resolve(activeManagerArr);
+    });
+  });
+
+  const getManagerList = new Promise((resolve, reject) => {
+    var managerArr = [];
+    const sql = ` SELECT concat(m.first_name, ' ', m.last_name) 
+                  AS manager FROM employees m `;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        console.log(err.message);
+      }
+      for (var i = 0; i < rows.length; i++) {
+        managerArr.push(Object.values(rows[i])[0]);
+      }
+      managerArr.push("Employee does not have a manager");
       resolve(managerArr);
     });
   });
   // we get the ids the same way
   const getManIdList = new Promise((resolve, reject) => {
     var manIdArr = [];
-    const sql = `SELECT e_id FROM employees WHERE is_manager = 1`;
+    const sql = ` SELECT DISTINCT m.e_id AS manager 
+                  FROM employees e, employees m 
+                  WHERE m.e_id = e.manager_id `;
     db.query(sql, (err, rows) => {
       if (err) {
         console.log(err.message);
@@ -333,7 +353,7 @@ function addEmployee() {
     });
   });
   // Promise.all([promises]) allows use to run multiple promises at once so we can pass through the data from each.
-  Promise.all([getTitles, getManagerList, getManIdList]).then(([titlesArr, managerArr, manIdArr]) => {
+  Promise.all([getTitles, getActiveManagerList, getManagerList, getManIdList]).then(([titlesArr, activeManagerArr, managerArr, manIdArr]) => {
     inquirer
       .prompt([
         {
@@ -368,71 +388,89 @@ function addEmployee() {
           message: "Choose the employee's role title",
           choices: titlesArr,
           filter: (roleIdInput) => {
-            if (roleIdInput === "Employee's manager not listed") {
-              return 0; // no one has an e_id of 0, so no manager will be assigned.
-            } else {
-              return titlesArr.indexOf(roleIdInput);
+            if (roleIdInput) {
+              return titlesArr.indexOf(roleIdInput) + 1;
             }
-          },
-        },
-        {
-          type: "confirm",
-          name: "isManagerConfirm",
-          message: "Is this employee a manager that oversees other employees?",
+          }
         },
         {
           type: "list",
-          name: "managerName",
+          name: "managerID1",
           message: "Select name of manager",
-          when: ({ isManagerConfirm }) => {
-            if (!isManagerConfirm) {
+          choices: activeManagerArr,
+          filter: managerID1Input => {
+            if (managerID1Input === "Show more") {
+              return managerID1Input;
+            } else {
+              return activeManagerArr.indexOf(managerID1Input);
+            }
+          }
+        },
+        {
+          type: "list",
+          name: "managerID2",
+          message: "Select name of manager",
+          choices: managerArr,
+          filter: managerID2Input => {
+            if (managerID2Input === "Employee does not have a manager") {
+              return 1; //defaults to CEO
+            } else {
+              return managerArr.indexOf(managerID2Input) + 1;
+            }
+          },
+          when: ({ managerID1 }) => {
+            if (isNaN(managerID1) === true) {
               return true;
             } else {
               return false;
             }
-          },
-          choices: managerArr,
+          }
         }
       ])
       .then(
-        ({ firstname, lastname, roleId, isManagerConfirm, managerName }) => {
-          const getIndex = () => {
-            return managerArr.indexOf(managerName); // we set up the manIdArr exactly the same as this array so we can use the index of this answer to get the corresponding manager_id
-          }
+        ({ firstname, lastname, roleId, managerID1, managerID2 }) => {          
           const getManId = () => {
-            return manIdArr.indexOf(getIndex);
+            if (isNaN(managerID1) === true) {
+              return managerID2; // returns 1,2,3... etc depending on user input 
+            } else {
+              return manIdArr[managerID1]; // we got the index of the validate statement of this question. 
+                                          //since the tables are set up the same way we can use the same index to get the ids stored in manIdArr
+            }
           }
+          const manId = getManId();
+          console.log(managerID2);
+          console.log(manId);
+
           const sql =
-            `INSERT INTO employees (first_name, last_name, role_id, is_manager, manager_name, manager_id) VALUES (?,?,?,?,?,?)`;
+            `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
           const query = [
             firstname,
             lastname,
-            (roleId + 1),
-            isManagerConfirm,
-            managerName,
-            getManId
+            roleId,
+            manId
           ]; 
           db.query(sql, query, (err, rows) => {
             if (err) {
               console.log(err.message);
+            } else {
+              console.log('');
+              console.log(`                                   Success!`);
+              inquirer
+              .prompt (
+                {
+                  type: 'confirm',
+                  name: 'results',
+                  message: 'See results?'
+                }
+              ).then(({ results }) => {
+                if (results) {
+                  console.log('');
+                  viewEmployees();
+                } else {
+                  homeMenu();
+                }
+              })
             }
-            console.log('');
-            console.log(`                                   Success!`);
-            inquirer
-            .prompt (
-              {
-                type: 'confirm',
-                name: 'results',
-                message: 'See results?'
-              }
-            ).then(({ results }) => {
-              if (results) {
-                console.log('');
-                viewEmployees();
-              } else {
-                homeMenu();
-              }
-            })
           });
         }
       );
